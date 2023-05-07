8f0d985ae9c5b9b1bf3bc49d2eed80b27d5bfe9c
kajalkapadiya <121010624+kajalkapadiya@users.noreply.github.com>
2023-05-07 17:09:14 +0530

    store data of different users on crud crud.

--- App.js
+++ App.js
@@ -1,43 +1,93 @@
 import "./App.css";
-import { Redirect, Route, Switch } from "react-router-dom";
-import MainHeader from "./Components/MainHeader";
-import Store from "./pages/Store";
-import About from "./pages/About";
-import Navigator from "./pages/Navigator";
-import ProductDetails from "./pages/ProductDetails";
-import Home from "./pages/Home";
-import LoginForm from "./Store/LoginForm";
+import MainHeader from "./Components/pages/MainHeader";
+import Cart from "./Components/Cart/Cart";
+import CartContextProvider from "./storeRoom/CartContextProvider";
+import { useState } from "react";
+import NavbarData from "./Components/pages/Navbar";
+import google from "./google.png";
+import spotify from "./spotify.png";
+import fb from "./fb.png";
 
 function App() {
+  const [cart, setCart] = useState(false);
+
+  const showCartHandler = () => {
+    setCart(true);
+  };
+
+  const closeCartHandler = () => {
+    setCart(false);
+  };
+
   return (
-    <div>
-      <MainHeader />
-      <main>
-        <Switch>
-          <Route path="/" exact>
-            <Redirect to="/home" />
-          </Route>
-          <Route path="/home">
-            <Home />
-          </Route>
-          <Route path="/products" exact>
-            <Store />
-          </Route>
-          <Route path="/about">
-            <About />
-          </Route>
-          <Route path="/LoginForm">
-            <LoginForm />
-          </Route>
-          <Route path="/navigator">
-            <Navigator />
-          </Route>
-          <Route path="/products/:productId">
-            <ProductDetails />
-          </Route>
-        </Switch>
-      </main>
-    </div>
+    <>
+      <CartContextProvider>
+        {cart && <Cart onClose={closeCartHandler} />}
+        <MainHeader onShowCart={showCartHandler} />
+        <div>
+          <h1
+            className="text-center p-3"
+            style={{
+              padding: "40px",
+              paddingTop: "40px",
+              paddingRight: "40px",
+              paddingBottom: "40px",
+              paddingLeft: "40px",
+              fontSize: "100px",
+              background: "gray",
+              color: "white",
+            }}
+          >
+            The Generics
+          </h1>
+        </div>
+        <NavbarData />
+      </CartContextProvider>
+      <footer
+        style={{
+          display: "flex",
+          marginTop: "40px",
+          padding: "20px",
+          background: "#56CCF2",
+        }}
+      >
+        <div
+          style={{
+            textAlign: "center",
+            color: "black",
+            width: "50%",
+            fontSize: "50px",
+            fontWeight: "bold",
+            margin: "0 auto",
+          }}
+        >
+          The Generics
+        </div>
+        <div
+          style={{
+            width: "50%",
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "space-around",
+          }}
+        >
+          <ul style={{ listStyleType: "none", display: "flex" }}>
+            <li style={{ padding: "10px", margin: "0px 20px" }}>
+              <a href="https://www.google.co.in/" />
+              <img src={google} style={{ width: "30px" }} />
+            </li>
+            <li style={{ padding: "10px", margin: "0px 20px" }}>
+              <a href="https://open.spotify.com/?" />
+              <img src={spotify} style={{ width: "30px" }} />
+            </li>
+            <li style={{ padding: "10px", margin: "0px 20px" }}>
+              <a href="https://www.facebook.com/" />
+              <img src={fb} style={{ width: "30px" }} />
+            </li>
+          </ul>
+        </div>
+      </footer>
+    </>
   );
 }
 
--- index.js
+++ index.js
@@ -5,11 +5,19 @@ import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter } from "react-router-dom";
 
+// React BootStrap configuration.
+
+import "../node_modules/react-bootstrap/dist/react-bootstrap";
+import "../node_modules/bootstrap/dist/css/bootstrap.css";
+import { AuthContextProvider } from "./Components/auth/AuthContext";
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+        <AuthContextProvider>
+          <App />
+        </AuthContextProvider>
     </BrowserRouter>
   </React.StrictMode>
 );
